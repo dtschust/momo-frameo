4cc8b6242fb7a4a27b866222334f3de7009d1a3c
Drew Schuster <dtschust@gmail.com>
2023-07-28 01:38:42 -0700

    updates

--- runSdScript.sh
+++ runSdScript.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
+echo "--------------------------------"
+date
+echo "Running SD script!"
 adb shell su 0 cp /mnt/media_rw/2307-0820/sd-card.sh /data/local/tmp
 adb shell su 0 chmod 777 /data/local/tmp/sd-card.sh
 adb shell su 0 "/data/local/tmp/sd-card.sh >> /data/local/tmp/log.txt"
