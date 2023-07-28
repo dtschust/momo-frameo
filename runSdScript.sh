591652e85952554053339f197937ae6a9ca86887
Drew Schuster <dtschust@gmail.com>
2023-07-28 04:36:49 -0400

    fixes

--- runSdScript.sh
+++ runSdScript.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 adb shell su 0 cp /mnt/media_rw/2307-0820/sd-card.sh /data/local/tmp
 adb shell su 0 chmod 777 /data/local/tmp/sd-card.sh
-adb shell su 0 /data/local/tmp/sd-card.sh >> /mnt/media_rw/2307-0820/log.txt
+adb shell su 0 "/data/local/tmp/sd-card.sh >> /data/local/tmp/log.txt"
+adb shell su 0 cp /data/local/tmp/log.txt /mnt/media_rw/2307-0820/
