bd5936edea88f483cb0a406fb52ef21e55322456
Drew Schuster <dtschust@gmail.com>
2023-07-28 01:10:08 -0700

    updates

--- gitUpdate.sh
+++ gitUpdate.sh
@@ -2,5 +2,8 @@
 PARENT_PATH=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
 cd "$PARENT_PATH"
 
+echo "--------------------------------"
+date
+echo "Updating git"
 git fetch http
 git reset --hard http/main
